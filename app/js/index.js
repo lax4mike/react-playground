4ef6530e1f8b4451b77313946f2a22d872d551fe
Mike Lambert <lax4mike@gmail.com>
2015-03-22 20:09:08 -0400

    Renaming resize files

--- app/js/index.js
+++ app/js/index.js
@@ -2,8 +2,8 @@
 // handle es6 > es5 compilation
 require("./es6/compiler");
 
-require("./es6/console-resize");
-require("./es6/code-resize");
+require("./es6/resize-console");
+require("./es6/resize-code");
 
 require("./es6/toggle-btns");
 
