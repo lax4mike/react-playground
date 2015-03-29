24499e4444f0ddeaea35e2a82e8ce5316b2e3f0f
Mike Lambert <lax4mike@gmail.com>
2015-03-29 06:33:52 -0400

    Better console log when logging objects

--- app/js/es6/console.js
+++ app/js/es6/console.js
@@ -6,7 +6,14 @@ window.console.log = (function() {
     var log = console.log;
     return function() {
         log.apply(window.console, arguments);
-        $(".console__text").append("<div  class='console__log'>" + Array.prototype.slice.call(arguments).join(" ") + 
+
+        // if an arg is an object, print out the JSON instead of [object Object]
+        var args = (Array.prototype.slice.call(arguments));
+        var argString = args.map(function(arg){
+            return (typeof arg  === "object") ? JSON.stringify(arg, null, 1) : arg.toString(); 
+        }).join(" ");
+
+        $(".console__text").append("<div  class='console__log'>" + argString + 
             "</div>");
     };
 }());
@@ -15,7 +22,14 @@ window.console.error = (function() {
     var err = console.error;
     return function() {
         err.apply(window.console, arguments);
-        $(".console__text").append("<div class='console__error'>" + Array.prototype.slice.call(arguments).join(" ") + 
+
+        // if an arg is an object, print out the JSON instead of [object Object]
+        var args = (Array.prototype.slice.call(arguments));
+        var argString = args.map(function(arg){
+            return (typeof arg  === "object") ? JSON.stringify(arg, null, 1) : arg.toString(); 
+        }).join(" ");
+
+        $(".console__text").append("<div class='console__error'>" + argString + 
             "</div>");
     };
 }());
