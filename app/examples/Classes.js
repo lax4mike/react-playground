fc75baa7ed722f1c38338d9b70520f302b5d638e
Jonathan Dallas <jonathan.dallas@velir.com>
2015-04-02 09:47:09 -0400

    added underscore

--- app/examples/Classes.js
+++ app/examples/Classes.js
@@ -1,1 +1,47 @@
 // Classes
+
+class View {
+  constructor(options) {
+    this.model = options.model;
+    this.template = options.template;
+  }
+
+  render() {
+    return _.template(this.template, this.model.toObject());
+  }
+}
+
+class Model {
+  constructor(properties) {
+    this.properties = properties;
+  }
+
+  toObject() {
+    return this.properties;
+  }
+}
+
+class LogView extends View {
+  render() {
+    var compiled = super.render();
+    console.log(compiled);
+  }
+}
+
+var jack = new Model({
+  name: 'jack'
+});
+
+var view = new View({
+  model: jack,
+  template: 'Hello, <%= name %>'
+});
+
+console.log(view.render());
+
+var logView = new LogView({
+  model: jack,
+  template: 'Hello, <%= name %>'
+});
+
+console.log(logView.render());
